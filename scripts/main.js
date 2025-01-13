2a0187d0696321d35716ceab36a63c419614caad
Zhenkaaf <zhenkaaf@gmail.com>
2025-01-13 13:17:35 +0200

    tabsGridAreas

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import Header from "./Header.js";
 import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js";
+import TabsCollection from "./Tabs.js";
 
 new Header();
+new TabsCollection();
 
 defineScrollBarWidthCSSVar();
