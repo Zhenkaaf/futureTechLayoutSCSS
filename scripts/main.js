d64ded7b1083ac43461bf151e25ee26b0c317513
Zhenkaaf <zhenkaaf@gmail.com>
2025-01-20 17:35:24 +0200

    contactsForm

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,13 @@ import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
 import ExpandableContentCollection from "./ExpandableContent.js";
 import InputMaskCollection from "./InputMask.js";
+import SelectCollection from "./Select.js";
+
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
 new ExpandableContentCollection();
 new InputMaskCollection();
+new SelectCollection();
 
 defineScrollBarWidthCSSVar();
